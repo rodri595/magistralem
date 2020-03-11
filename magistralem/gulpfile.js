61dc5733f50ceca43a264226ae020ed8676bfc37
rodri595 <rodri.595@hotmail.com>
2020-03-11 14:43:56 -0600

    fix gulp function

--- magistralem/gulpfile.js
+++ magistralem/gulpfile.js
@@ -1,27 +1,24 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var minifyCSS = require('gulp-clean-css');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var changed = require('gulp-changed');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var minifyCSS = require("gulp-clean-css");
+var rename = require("gulp-rename");
+var changed = require("gulp-changed");
 
+var SCSS_SRC = "./src/Assets/scss/**/*.scss";
+var SCSS_DEST = "./src/Assets/css";
 
-
-var SCSS_SRC = './src/Assets/scss/**/*.scss';
-var SCSS_DEST = './src/Assets/css';
-
-
-function style(){
-  return gulp.src(SCSS_SRC)
-  .pipe(sass().on('error', sass.logError))
-  .pipe(minifyCSS())
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(changed(SCSS_DEST))
-  .pipe(gulp.dest(SCSS_DEST))
+function style() {
+  return gulp
+    .src(SCSS_SRC)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(changed(SCSS_DEST))
+    .pipe(gulp.dest(SCSS_DEST));
 }
 
 function watch() {
-  gulp.watch('./src/Assets/scss/**/*.scss', style);
+  gulp.watch("./src/Assets/scss/**/*.scss", style);
 }
 
 gulp.task("default", watch);
